7086815f30901eb40ce571ef255f8b168c85d40f
SanjayKumar <0508sanjaykumar@gmail.com>
2025-04-10 10:30:01 +0530

    feat: Added player

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Player;
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, player_movement_system)
         .run();
 }
 
@@ -12,14 +16,17 @@ fn setup(mut commands: Commands) {
     // Camera
     commands.spawn(Camera2dBundle::default());
 
-    // Player
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.3, 0.7, 0.9),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.3, 0.7, 0.9),
+                custom_size: Some(Vec2::new(50.0, 50.0)),  // size of player square
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, -200.0, 0.0),  // initial position
             ..default()
         },
-        transform: Transform::from_xyz(0.0, -200.0, 0.0),
-        ..default()
-    });
+        Player, // add marker component
+    ));
 }
+
