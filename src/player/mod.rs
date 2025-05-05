5125160f186344077809524ff74be68c624a8e35
SanjayKumar <0508sanjaykumar@gmail.com>
2025-05-05 16:36:46 +0530

    refactor: Moved into a different file

---
+++ src/player/mod.rs
@@ -0,0 +1,5 @@
+mod spawn;
+mod movement;
+
+pub use spawn::spawn_player;
+pub use movement::player_movement_system;
--- src/player/movement.rs
+++ src/player/movement.rs
@@ -4,26 +4,6 @@ use bevy::input::keyboard::KeyCode;
 
 use crate::components::{Player, Velocity, PlayerState};
 
-pub fn spawn_player(mut commands: Commands) {
-    commands.spawn((
-        Sprite {
-            color: Color::srgb(0.3, 0.7, 0.9),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
-            ..default()
-        },
-        Transform::from_xyz(0.0, -200.0, 0.0),
-        Visibility::Visible,
-        Player,
-        Velocity { y: 0.0 },
-        PlayerState {
-            on_ground: true,
-            coyote_timer: 0.0,
-            jump_buffer_timer: 0.0,
-        },
-
-    ));
-}
-
 pub fn player_movement_system(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     mut query: Query<(&mut Transform, &mut Velocity, &mut PlayerState), With<Player>>,
